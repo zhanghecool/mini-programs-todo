7ee02c6cdf5072ba6a8001ad3732dde52a8403d3
He Zhang <zhanghecool@outlook.com>
2020-09-21 16:50:00 +0800

    feat(theme.json): 增加主题色，增加layout组件

---
+++ miniprogram/components/NavigationBar/NavigationBar.js
@@ -0,0 +1,24 @@
+// components/NavigationBar/NavigationBar.js
+// 如果app.json中声明了"darkmode": true，wx.getSystemInfo或wx.getSystemInfoSync的返回结果中会包含theme属性，值为light或dark。
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
